8466f053874a4e7b96f85ca484311b4c8a3e13d9
gedgaudasnikita <gedgaudasnikita@gmail.com>
2017-09-21 19:51:37 +0300

    First dummy Translator and basic test data

---
+++ Comparison-shopping-engine/ReceiptTranslator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace Comparison_shopping_engine
+{
+    public static class ReceiptTranslator
+    {
+        private static TesseractEngine engine = new TesseractEngine(@"./tessdata", "lit", EngineMode.Default);
+
+        public static String convertToText(Bitmap receipt)
+        {
+            PixConverter.ToPix(receipt);
+            var text = engine.Process(receipt).GetText();
+            return text;
+        }
+    }
+}
