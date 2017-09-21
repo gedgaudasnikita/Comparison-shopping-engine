4ee9bafc9a53999821ea7ba9714da5fbdbb9f422
gedgaudasnikita <gedgaudasnikita@gmail.com>
2017-09-21 17:48:53 +0300

    Create Item and ItemManager class

---
+++ Comparison-shopping-engine/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine
+{
+    public class Item
+    {
+        private string name;
+        private string store;
+        //Vienos prakės kaina arba 1kg kaina jei prekė sveriama
+        private double price;
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Store
+        {
+            get; set;
+        }
+
+        public double Price
+        {
+            get; set;
+        }
+
+        public Item()
+        {
+            this.name = "";
+            this.store = "";
+            this.price = 0;
+        }
+
+        public Item(string name, string store, double price)
+        {
+            this.name = name;
+            this.store = store;
+            this.price = price;
+        }
+
+        public void print()
+        {
+            Console.Out.WriteLine(name + " " + store + " " + price.ToString());
+        }
+    }
+}
