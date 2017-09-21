7c72fab74641fc131510f07b0ce8ba3681c1a9f8
juras.jankauskas <juras.jankauskas@gmail.com>
2017-09-21 17:48:53 +0300

    Create Item and ItemManager class

---
+++ Comparison-shopping-engine/ItemManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine
+{
+    static public class ItemManager
+    {
+        //Palygina dvi prekes ir grąžiną pigesnę, jeigu kainos sutampa grąžina prekę a
+        static public Item comparePrice (Item a, Item b)
+        {
+            if (b.Price < a.Price) return b;
+            else return a;
+        }
+    }
+}
