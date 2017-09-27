e4415d1216ad051a44387484d2e12a4a8387b84d
juras.jankauskas <juras.jankauskas@gmail.com>
2017-09-27 22:03:49 +0300

    Set up basic structure for comparison logic Rename ItemManagerto ItemComparer Create ItemManager class

--- Comparison-shopping-engine/ItemManager.cs
+++ Comparison-shopping-engine/ItemManager.cs
@@ -6,32 +6,8 @@ using System.Threading.Tasks;
 
 namespace Comparison_shopping_engine
 {
-    static public class ItemManager
+    //Klasė atsakinga už Item objektų sarašą, jo tvarkymą ir jo rašymą/skaitymą iš failo
+    class ItemManager
     {
-        //Pasako ar prekių pavadinimai lygūs, palyginimas case-sensitive
-        static public bool compareName(Item a, Item b)
-        {
-            return a.Name.Equals(b.Name, StringComparison.Ordinal);
-        }
-
-        //Pasako ar prekių parduotuvių pavadinimai lygūs, palyginimas case-sensitive
-        static public bool compareStore(Item a, Item b)
-        {
-            return a.Store.Equals(b.Store, StringComparison.Ordinal);
-        }
-
-        //Palygina prekes pagal kainą ir gražina pigesnę prekę, jeigu kainos vienodos, grąžina prekę a
-        static public Item comparePrice(Item a, Item b)
-        {
-            if (a.Price > b.Price) return b;
-            else return a;
-        }
-
-        //Palygina prekes pagal datą ir gražina naujesnę prekę, jeigu datos vienodos, grąžina prekę a
-        static public Item compareDate(Item a, Item b)
-        {
-            if (a.Date < b.Date) return b;
-            else return a;
-        }
     }
 }
