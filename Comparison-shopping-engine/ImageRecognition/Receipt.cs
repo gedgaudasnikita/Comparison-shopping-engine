dab5466699ab1d964bf122eb197356abd991d1a7
gedgaudasnikita <gedgaudasnikita@gmail.com>
2017-10-05 17:58:56 +0300

    A more OOP valid approach

---
+++ Comparison-shopping-engine/ImageRecognition/Receipt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine
+{
+    class Receipt
+    {
+        private string store;
+        private List<Item> items;
+
+        private static IParser<string> storeParser;
+        private static IParser<List<Item>> itemListParser;
+
+        public string Store { get; set; }
+        public List<Item> Items { get; set; }
+
+        public static IParser<string> StoreParser { private get; set; }
+        public static IParser<List<Item>> ItemListParser { private get; set; }
+
+        public static Receipt Parse(Bitmap source)
+        {
+            var text = OCRWrapper.ConvertToText(source);
+            return Parse(text);
+        }
+
+        public static Receipt Parse(string source)
+        {
+            Receipt result = new Receipt();
+
+            result.Store = storeParser.Parse(source);
+            result.Items = itemListParser.Parse(source);
+
+            return result;
+        }
+
+    }
+}
