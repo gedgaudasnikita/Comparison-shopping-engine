683fa2ec3f622db44fd2a6713a1f5459334ff0d0
Nik <Nik@LENOVO-PC>
2017-10-05 17:58:56 +0300

    A more OOP valid approach

---
+++ Comparison-shopping-engine/ImageRecognition/ItemListParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine
+{
+    class ItemListParser: IParser<List<Item>>
+    {
+        //Lines ending with three numbers and tax groups A or M1 are what we consider items
+        private string itemRegex = @"(.+)(\d.*\d.*\d.*(A|M1))";
+
+        public List<Item> Parse(string source)
+        {
+            List<Item> parsedItems = new List<Item>();
+
+            var itemMatch = Regex.Match(source, itemRegex);
+
+            while (itemMatch.Success)
+            {
+                parsedItems.Add(ParseItem(itemMatch));
+                itemMatch = itemMatch.NextMatch();
+            }
+
+            return parsedItems;
+        }
+
+        private Item ParseItem(Match matchedItem)
+        {
+            Item parsedItem = new Item();
+
+            string itemName = matchedItem.Groups[1].Value;
+
+            int itemPrice = 0;
+            string itemPriceClean = matchedItem.Groups[2].Value.RemoveNonDigits();
+            int.TryParse(itemPriceClean, out itemPrice);
+
+            return parsedItem;
+        }
+    }
+}
---
+++ Comparison-shopping-engine/ImageRecognition/StoreParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine
+{
+    public class StoreParser: IParser<string>
+    {
+        private int maximumMatchingDistance = 7;
+
+        private string[] supportedStores;
+        private int minimumStoreNameLength;
+        private int maximumStoreNameLength;
+
+        public StoreParser()
+        {
+            int.TryParse(ConfigurationManager.AppSettings["maximumMatchingDistance"], out maximumMatchingDistance);
+            supportedStores = ConfigurationManager.AppSettings["supportedStores"].Split(',');
+            minimumStoreNameLength = supportedStores.Min(storeName => storeName.Length);
+            maximumStoreNameLength = supportedStores.Max(storeName => storeName.Length);
+        }
+
+        public string Parse(string source)
+        {
+            string parsedStore = "";
+
+            //Larger distances give virtually random results, better consider the store unparseable
+            int minScore = maximumMatchingDistance;
+
+            //We iterate through the whole source, because there is no guaranteed placement of the store name
+            for (int index = 0; index < source.Length; index++)
+            {
+                //Matching all possible substrings is inefficient, limit that to relevants only
+                for (int length = minimumStoreNameLength; length < maximumStoreNameLength && length + index < source.Length; length++)
+                {
+                    //Calculate a matching score for each store name; smallest wins
+                    foreach(string store in supportedStores)
+                    {
+                        int currentScore = store.GetDistance(source.Substring(index, length));
+
+                        if (currentScore < minScore)
+                        {
+                            minScore = currentScore;
+                            parsedStore = store;
+                        }
+                    }
+                }
+            }
+
+            return parsedStore;
+        }
+
+    }
+}
