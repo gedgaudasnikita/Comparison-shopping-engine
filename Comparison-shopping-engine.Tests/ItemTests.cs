efb5c50a0dc745205446796513dfa0bd3f4a7152
gedgaudasnikita <gedgaudasnikita@gmail.com>
2017-10-05 13:21:35 +0300

    Unit Tests for Item, ItemComparer

---
+++ Comparison-shopping-engine.Tests/ItemTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Comparison_shopping_engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine.Tests
+{
+    [TestClass()]
+    public class ItemTests
+    {
+        [TestMethod()]
+        public void ItemTest()
+        {
+            Item i = new Item("Name", "Store", 9.99, DateTime.Now);
+            Assert.IsNotNull(i);
+        }
+
+        [TestMethod()]
+        public void ItemTest1()
+        {
+            Item i = new Item("Name", "Store", 9.99, "2017-10-10");
+            Assert.IsNotNull(i);
+        }
+
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
