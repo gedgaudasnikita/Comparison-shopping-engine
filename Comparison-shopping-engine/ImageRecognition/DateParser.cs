d9046a0f88097bbbdb211b6104d4a5a4190aef15
gedgaudasnikita <gedgaudasnikita@gmail.com>
2017-10-06 16:15:02 +0300

    Add DateParser

--- Comparison-shopping-engine/ImageRecognition/DateParser.cs
+++ Comparison-shopping-engine/ImageRecognition/DateParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Comparison_shopping_engine
@@ -10,10 +11,10 @@ namespace Comparison_shopping_engine
     /// This class describes the behaviour of parsing an date from plain text.
     /// Intended for usage from <see cref="Receipt"> class
     /// </summary>
-    class DateParser: IParser<DateTime>
+    public class DateParser: IParser<DateTime>
     {
-        //Lines ending with three numbers and tax groups A or M1 are what we consider items
-        private string dateRegex = @"";
+        //Basically searching for 8 digits and whatever (of reasonable amount) rubbish inbetween them
+        private string dateRegex = @"(\d\D{0,3}\d\D{0,3}\d\D{0,3}\d\D{0,3}\d\D{0,3}\d\D{0,3}\d\D{0,3}\d)";
 
         /// <summary>
         /// Parses the given <see langword="string"/> value and returns the date.
@@ -26,14 +27,36 @@ namespace Comparison_shopping_engine
         /// </returns>
         public DateTime Parse(string source)
         {
-            DateTime parsedDate = new DateTime();
-
-            var itemMatch = Regex.Match(source, dateRegex);
-
             //If our regex or parsing did not work, assume the default is current date
-            if (!(itemMatch.Success && DateTime.TryParse(itemMatch.Value, out parsedDate)))
+            DateTime parsedDate = DateTime.Now.Date;
+
+            long minDifference = long.MaxValue;
+
+            var dateMatch = Regex.Match(source, dateRegex);
+
+            //Since we can't do better than to simply look for 9 numbers, there might be 
+            //a lot of wrong numbers involved. We have to check throught them all, and find
+            //not only the valid, but also the most believable (closest the to current date)
+            while (dateMatch.Success)
             {
-                    parsedDate = DateTime.Now;
+                DateTime tempDate = new DateTime();
+                if (DateTime.TryParseExact(dateMatch.Value.RemoveNonDigits(), 
+                    //All lithuanian dates follow this format, ignoring the separators
+                    "yyyyMMdd", 
+                    System.Globalization.CultureInfo.InvariantCulture,
+                    System.Globalization.DateTimeStyles.None, 
+                    out tempDate)) 
+                {
+                    long currentDifference = (parsedDate - DateTime.Now.Date).Ticks;
+
+                    if (currentDifference < minDifference)
+                    {
+                        minDifference = currentDifference;
+                        parsedDate = tempDate;
+                    }
+                }
+
+                dateMatch = dateMatch.NextMatch();
             }
 
             //We really dont need the Time part in our internal calculations
