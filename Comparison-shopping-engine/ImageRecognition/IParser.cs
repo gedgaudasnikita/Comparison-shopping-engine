ec8eb469ebd267dab39afa3d0b9206ad29fecf18
gedgaudasnikita <gedgaudasnikita@gmail.com>
2017-10-06 13:48:47 +0300

    Project wide comments and DateParser w/o regex

--- Comparison-shopping-engine/ImageRecognition/IParser.cs
+++ Comparison-shopping-engine/ImageRecognition/IParser.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Comparison_shopping_engine
 {
+    /// <summary>
+    /// Interface to be implemented by all the <see cref="Receipt"> field parsers.
+    /// The generic type T is the type of the respective <see cref="Receipt"> field.
+    /// </summary>
     public interface IParser<T>
     {
+        /// <summary>
+        /// Parses the given <see langword="string"/> value and returns the <see cref="Receipt"> field
+        /// </summary>
+        /// <param name="source">The <see langword="string"/> to be parsed.</param>
+        /// <returns>
+        /// Returns a value of type T, containing a parsed value, implementation specific
+        /// </returns>
         T Parse(string source);
     }
 }
