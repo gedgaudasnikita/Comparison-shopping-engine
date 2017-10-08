8df0869de4c81b6fe1866b60a78bf61586d1f960
DESKTOP-LAA12VO\aaaa <odeta.kizyte@gmail.com>
2017-10-08 20:03:40 +0300

    Added basic WF interface

---
+++ Comparison-shopping-engine/mainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Comparison_shopping_engine
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Initializes all components in a form
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_NewReceipt = new System.Windows.Forms.Button();
+            this.lbl_ReceptInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_NewReceipt
+            // 
+            this.btn_NewReceipt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_NewReceipt.Location = new System.Drawing.Point(204, 343);
+            this.btn_NewReceipt.Name = "btn_NewReceipt";
+            this.btn_NewReceipt.Size = new System.Drawing.Size(187, 28);
+            this.btn_NewReceipt.TabIndex = 0;
+            this.btn_NewReceipt.Text = "Upload new receipt";
+            this.btn_NewReceipt.UseVisualStyleBackColor = true;
+            this.btn_NewReceipt.Click += new System.EventHandler(this.Btn_NewReceipt_Click);
+            // 
+            // lbl_ReceptInfo
+            // 
+            this.lbl_ReceptInfo.AutoSize = true;
+            this.lbl_ReceptInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl_ReceptInfo.Location = new System.Drawing.Point(0, 0);
+            this.lbl_ReceptInfo.Name = "lbl_ReceptInfo";
+            this.lbl_ReceptInfo.Padding = new System.Windows.Forms.Padding(10);
+            this.lbl_ReceptInfo.Size = new System.Drawing.Size(174, 33);
+            this.lbl_ReceptInfo.TabIndex = 1;
+            this.lbl_ReceptInfo.Text = "Receipt information will be here";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 383);
+            this.Controls.Add(this.lbl_ReceptInfo);
+            this.Controls.Add(this.btn_NewReceipt);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comparison Shopping Engine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button btn_NewReceipt;
+        private System.Windows.Forms.Label lbl_ReceptInfo;
+    }
+}
---
+++ Comparison-shopping-engine/mainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comparison_shopping_engine
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Reacts to button <see cref="btn_NewReceipt"></see> click by opening a <see cref="OpenFileDialog"/>.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_NewReceipt_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // code to read the stream
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
