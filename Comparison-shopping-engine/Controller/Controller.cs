3b0db8d1c018db138678ae0ff69c396e77faf9ed
juras.jankauskas <juras.jankauskas@gmail.com>
2017-10-11 17:13:35 +0300

    Create Controller class

---
+++ Comparison-shopping-engine/Controller/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine.Controller
+{
+    public static class Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static Receipt ProcessReceipt(Bitmap source)
+        {
+            Receipt receipt = Receipt.Convert(source);
+            return ProcessReceipt(receipt);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static Receipt ProcessReceipt(Receipt source)
+        {
+            ItemManager manager = ItemManager.GetInstance();
+            return source;
+        }
+    }
+}
