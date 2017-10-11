1baeb1d463e2d98a5127903416ac97eaadea9720
juras.jankauskas <juras.jankauskas@gmail.com>
2017-10-11 17:39:21 +0300

    Create mock dummy unit tests

---
+++ Comparison-shopping-engine.Tests/Controller/ControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Comparison_shopping_engine.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison_shopping_engine.Controller.Tests
+{
+    [TestClass()]
+    public class ControllerTests
+    {
+        [TestMethod()]
+        public void ProcessReceiptTest()
+        {
+            //TO DO
+            //Test with different receipt pictures
+            Assert.Inconclusive();
+        }
+
+        [TestMethod()]
+        public void ProcessReceiptTest1()
+        {
+            //TO DO
+            //Test with receipt examples
+            Assert.Fail();
+        }
+    }
+}
