9588c08f8c4143e339f4ac325beaac057bbe9b95
odeta <31925863+odeta@users.noreply.github.com>
2017-10-12 23:50:19 +0300

    Updated Program.cs

--- Comparison-shopping-engine/Program.cs
+++ Comparison-shopping-engine/Program.cs
@@ -16,7 +16,7 @@ namespace Comparison_shopping_engine
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new mainForm());
+            Application.Run(new MainForm());
         }
     }
 }
