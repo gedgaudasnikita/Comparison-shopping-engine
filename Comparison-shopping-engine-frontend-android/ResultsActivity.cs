16a84f319432883e60abac2537093670b7f909d0
DESKTOP-LAA12VO\aaaa <odeta.kizyte@gmail.com>
2017-11-17 02:28:53 +0200

    Fix

--- Comparison-shopping-engine-frontend-android/ResultsActivity.cs
+++ Comparison-shopping-engine-frontend-android/ResultsActivity.cs
@@ -66,7 +66,7 @@ namespace Comparison_shopping_engine_frontend_android
 
         private async void ProcessReceipt()
         {
-            string receiptText = Intent.GetStringExtra("ReceiptText") ?? ("NoReceipt");
+            string receiptText = Intent.GetStringExtra("ReceiptText") ?? ("");
             
             BackendInterface backendIterface = new BackendInterface();
             Receipt receiptToProcess = await backendInterface.ProcessImage(receiptText);
