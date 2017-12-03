78541c49f66321f7fe38fd8893790dd208b21db8
gedgaudasnikita <gedgaudasnikita@gmail.com>
2017-12-03 18:36:02 +0200

    Update build-doc.sh

--- build-doc.sh
+++ build-doc.sh
@@ -21,10 +21,12 @@ echo "" > .nojekyll
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
 if [ -d "html" ] && [ -f "html/index.html" ]; then
-	mv html/* .
-	rm -rf html
-	git add --all
-	git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+    echo 'Documentation created'
+    mv html/* .
+    rm -rf html
+    echo 'Comitting'
+    git add --all
+    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
 	
     git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 else
