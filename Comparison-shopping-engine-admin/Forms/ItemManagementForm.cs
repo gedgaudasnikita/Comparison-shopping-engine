5cb8e50818cf365dcdb5affa0b670ecb3fa947ad
Nikita Gedgaudas <gedgaudasnikita@gmail.com>
2017-12-04 20:34:14 +0200

    Add barebone structure

---
+++ Comparison-shopping-engine-admin/Forms/ItemManagementForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comparison_shopping_engine_admin
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ItemDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+    }
+}
