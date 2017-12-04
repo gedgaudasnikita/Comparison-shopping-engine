8f37c7ae83d0966a0de59bb7015438ccf35d35db
Nikita Gedgaudas <gedgaudasnikita@gmail.com>
2017-12-05 01:25:48 +0200

    Implement code coverage

--- Comparison-shopping-engine-documentation/build-doc.sh
+++ Comparison-shopping-engine-documentation/build-doc.sh
@@ -2,9 +2,9 @@
 
 #Original: https://gist.github.com/vidavidorra/548ffbcdae99d752da02
 
-if [ $TRAVIS_BRANCH != 'master' ]; then
-	exit 0
-fi
+#if [ $TRAVIS_BRANCH != 'master' ]; then
+#	exit 0
+#fi
 
 set -e
 
@@ -22,6 +22,9 @@ rm -rf *
 
 echo "" > .nojekyll
 echo ${TRAVIS_BUILD_NUMBER} > .version
+
+coveragerunner/OpenCover.Console.exe -target:"../../../testrunner/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe" -targetargs:"../../../Comparison-shopping-engine-backend.Tests/bin/CI/Comparison-shopping-engine-backend.Tests.dll" -filter:"+[*]* -[Comparison-shopping-engine-backend.Tests*]*" -register
+coveragereporter/ReportGenerator.exe -reports:results.xml -targetdir:coverage
 
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
