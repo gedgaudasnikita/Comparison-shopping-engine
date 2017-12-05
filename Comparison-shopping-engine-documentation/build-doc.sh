6401f1fbe5cc0114f0ff37a8f8b93e117e25a5f3
Nikita Gedgaudas <gedgaudasnikita@gmail.com>
2017-12-05 19:36:30 +0200

    Slight updates

--- Comparison-shopping-engine-documentation/build-doc.sh
+++ Comparison-shopping-engine-documentation/build-doc.sh
@@ -2,30 +2,11 @@
 
 #Original: https://gist.github.com/vidavidorra/548ffbcdae99d752da02
 
-#if [ $TRAVIS_BRANCH != 'master' ]; then
+#if [ $APPVEYOR_REPO_BRANCH != 'master' ]; then
 #	exit 0
 #fi
 
-export LD_LIBRARY_PATH=/usr/local/lib
-mono --debug --profile=monocov:outfile=monocovCoverage.cov,+Comparison-shopping-engine-backend ./testrunner/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe --process=Single ./Comparison-shopping-engine-backend.Tests/bin/CI/Comparison-shopping-engine-backend.Tests.dll
-monocov --export-xml=monocovCoverage monocovCoverage.cov
-cat monocovCoverage.cov
-ls monocovCoverage
-REPO_COMMIT_AUTHOR=$(git show -s --pretty=format:"%cn")
-REPO_COMMIT_AUTHOR_EMAIL=$(git show -s --pretty=format:"%ce")
-REPO_COMMIT_MESSAGE=$(git show -s --pretty=format:"%s")
-echo $TRAVIS_COMMIT
-echo $TRAVIS_BRANCH
-echo $REPO_COMMIT_AUTHOR
-echo $REPO_COMMIT_AUTHOR_EMAIL
-echo $REPO_COMMIT_MESSAGE
-echo $TRAVIS_JOB_ID
-
-
 set -e
-
-mono ./packages/coveralls.net.0.6.0/tools/csmacnz.Coveralls.exe --monocov -i ./monocovCoverage --repoToken $COVERALLS_REPO_TOKEN --commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor "$REPO_COMMIT_AUTHOR" --commitEmail "$REPO_COMMIT_AUTHOR_EMAIL" --commitMessage "$REPO_COMMIT_MESSAGE" --jobId $TRAVIS_JOB_ID  --serviceName travis-ci  --useRelativePaths
- 
 
 mkdir code_docs
 cd code_docs
@@ -34,13 +15,13 @@ git clone -b gh-pages https://git@$GH_REPO_REF
 cd $GH_REPO_NAME
 
 git config --global push.default simple
-git config user.name "Travis CI"
-git config user.email "travis@travis-ci.org"
+git config user.name "Appveyor CI"
+git config user.email "appveyor@appveyor.org"
 
 rm -rf *
 
 echo "" > .nojekyll
-echo ${TRAVIS_BUILD_NUMBER} > .version
+echo ${APPVEYOR_BUILD_NUMBER} > .version
 
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
@@ -48,7 +29,7 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     mv html/* .
     rm -rf html
     git add --all
-    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+    git commit -m "Deploy code docs to GitHub Pages Appveyor build: ${APPVEYOR_BUILD_NUMBER}" -m "Commit: ${APPVEYOR_REPO_COMMIT}"
 	
     git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 else
