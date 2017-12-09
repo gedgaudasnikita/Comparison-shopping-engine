6eb3736f2ca006c5a25bba1438232a1574e35d25
Odeta Kizytė <odeta.kizyte@gmail.com>
2017-12-09 18:40:46 +0200

    Creating fragment

--- src/FrontendAndroid/Resources/Resource.Designer.cs
+++ src/FrontendAndroid/Resources/Resource.Designer.cs
@@ -71,50 +71,53 @@ namespace Comparison_shopping_engine_frontend_android
 			// aapt resource value: 0x7f050001
 			public const int configTextView = 2131034113;
 			
-			// aapt resource value: 0x7f050007
-			public const int homeCameraButton = 2131034119;
-			
 			// aapt resource value: 0x7f05000a
-			public const int homeConfigButton = 2131034122;
+			public const int fragmentLinearLayout = 2131034122;
 			
-			// aapt resource value: 0x7f050008
-			public const int homeGalleryButton = 2131034120;
+			// aapt resource value: 0x7f050006
+			public const int homeCameraButton = 2131034118;
 			
-			// aapt resource value: 0x7f050005
-			public const int homeImageView = 2131034117;
+			// aapt resource value: 0x7f050009
+			public const int homeConfigButton = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int homeGalleryButton = 2131034119;
+			
+			// aapt resource value: 0x7f05000b
+			public const int homeImageView = 2131034123;
 			
 			// aapt resource value: 0x7f050003
 			public const int homeLinearLayout = 2131034115;
 			
-			// aapt resource value: 0x7f050009
-			public const int homeResultScreenButton = 2131034121;
+			// aapt resource value: 0x7f050008
+			public const int homeResultScreenButton = 2131034120;
 			
 			// aapt resource value: 0x7f050004
 			public const int homeTextView = 2131034116;
 			
-			// aapt resource value: 0x7f050006
-			public const int homeVLinearLayout = 2131034118;
-			
-			// aapt resource value: 0x7f05000e
-			public const int itemsLinearLayout = 2131034126;
+			// aapt resource value: 0x7f050005
+			public const int homeVLinearLayout = 2131034117;
 			
 			// aapt resource value: 0x7f05000f
-			public const int resultsButtonsLinearLayout = 2131034127;
-			
-			// aapt resource value: 0x7f05000b
-			public const int resultsLinearLayout = 2131034123;
+			public const int itemsLinearLayout = 2131034127;
 			
 			// aapt resource value: 0x7f050010
-			public const int resultsNewItemButton = 2131034128;
-			
-			// aapt resource value: 0x7f05000d
-			public const int resultsScroll = 2131034125;
-			
-			// aapt resource value: 0x7f050011
-			public const int resultsSubmitButton = 2131034129;
+			public const int resultsButtonsLinearLayout = 2131034128;
 			
 			// aapt resource value: 0x7f05000c
-			public const int resultsTextView = 2131034124;
+			public const int resultsLinearLayout = 2131034124;
+			
+			// aapt resource value: 0x7f050011
+			public const int resultsNewItemButton = 2131034129;
+			
+			// aapt resource value: 0x7f05000e
+			public const int resultsScroll = 2131034126;
+			
+			// aapt resource value: 0x7f050012
+			public const int resultsSubmitButton = 2131034130;
+			
+			// aapt resource value: 0x7f05000d
+			public const int resultsTextView = 2131034125;
 			
 			static Id()
 			{
@@ -136,7 +139,10 @@ namespace Comparison_shopping_engine_frontend_android
 			public const int Home = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int Results = 2130837506;
+			public const int PictureFragment = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int Results = 2130837507;
 			
 			static Layout()
 			{
