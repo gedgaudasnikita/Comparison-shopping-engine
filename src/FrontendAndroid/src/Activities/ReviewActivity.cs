ceebc7cf81f31178e0c89b03418abf51dcfc4a13
Odeta Kizytė <odeta.kizyte@gmail.com>
2017-12-14 21:02:30 +0200

    Separate Results activity into to two activities

---
+++ src/FrontendAndroid/src/Activities/ReviewActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Comparison_shopping_engine_frontend_android.src.Activities
+{
+    [Activity(Label = "ReviewActivity")]
+    public class ReviewActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
