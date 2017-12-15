f2ef5f5c79a5e59c881778af67e6cd41149e4f14
AppVeyor CI <team@appveyor.com>
2017-12-14 23:18:51 -0800

    Deploy code docs to GitHub Pages Appveyor build:210

--- search/classes_f.js
+++ search/classes_f.js
@@ -4,7 +4,7 @@ var searchData=
   ['savereceiptendpoint',['SaveReceiptEndpoint',['../d6/d80/classComparison__shopping__engine__web__protocol_1_1SaveReceiptEndpoint.html',1,'Comparison_shopping_engine_web_protocol']]],
   ['statecolormanager',['StateColorManager',['../d1/d23/classComparison__shopping__engine__frontend__android_1_1StateColorManager.html',1,'Comparison_shopping_engine_frontend_android']]],
   ['storeparser',['StoreParser',['../db/db1/classComparison__shopping__engine__backend_1_1StoreParser.html',1,'Comparison_shopping_engine_backend']]],
-  ['stringextension',['StringExtension',['../db/d1f/classComparison__shopping__engine__core__entities_1_1StringExtension.html',1,'StringExtension'],['../db/d4d/classComparison__shopping__engine__backend_1_1StringExtension.html',1,'StringExtension']]],
+  ['stringextension',['StringExtension',['../db/d4d/classComparison__shopping__engine__backend_1_1StringExtension.html',1,'StringExtension'],['../db/d1f/classComparison__shopping__engine__core__entities_1_1StringExtension.html',1,'StringExtension']]],
   ['suggestionadapter',['SuggestionAdapter',['../d3/d15/classComparison__shopping__engine__frontend__android_1_1SuggestionAdapter.html',1,'Comparison_shopping_engine_frontend_android']]],
   ['suggestionfilter',['SuggestionFilter',['../de/d12/classComparison__shopping__engine__frontend__android_1_1SuggestionAdapter_1_1SuggestionFilter.html',1,'Comparison_shopping_engine_frontend_android::SuggestionAdapter']]]
 ];
